e0044d1e15d60df9e734ad6c73cf75578e4a0443
Mikkel <mikkelhansen.92@jubii.dk>
2018-12-05 15:20:16 +0100

    Byte array to Image converter

---
+++ Common/Utils/ImageUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Common.Utils
+{
+    class ImageUtility
+    {
+        public Image ConvertByteArrayToImage(byte[] imageBytes)
+        {
+            Image image = null;
+            try
+            {
+                MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+                ms.Write(imageBytes, 0, imageBytes.Length);
+                image = Image.FromStream(ms, true);
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return image;
+        }
+    }
+}
