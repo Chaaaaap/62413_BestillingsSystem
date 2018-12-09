cf6c1bea13ea8fd49d443b1847d879c423b72b69
Mikkel <mikkelhansen.92@jubii.dk>
2018-12-10 00:27:32 +0100

    Lorte events der ikke virker

---
+++ DesktopClient/Events/EventPublisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopClient.Events
+{
+    public class EventPublisher : IEventPublisher
+    {
+        private readonly ConcurrentDictionary<Type, object> _subjects
+            = new ConcurrentDictionary<Type, object>();
+
+        public IObservable<TEvent> GetEvent<TEvent>()
+        {
+            //var subject =
+            //    (ISubject<TEvent>) _subjects.GetOrAdd(typeof(TEvent),
+            //        t => new Subject<TEvent>());
+            //return subject.AsObservable();
+            return null;
+        }
+
+        public void Publish<TEvent>(TEvent e)
+        {
+            object subject;
+            //if (_subjects.TryGetValue(typeof(TEvent), out subject))
+            //{
+            //    ((ISubject<TEvent>)subject)
+            //        .OnNext(e);
+            //}
+        }
+    }
+}
---
+++ DesktopClient/Events/IEventPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopClient.Events
+{
+    public interface IEventPublisher
+    {
+        void Publish<TEvent>(TEvent e);
+        IObservable<TEvent> GetEvent<TEvent>();
+    }
+}
